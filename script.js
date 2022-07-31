f20723287fe8234561e0842b45a7b101456af4e2
alan Calveyra <alancalveyra6@hotmail.com>
2022-07-30 22:05:34 -0300

    3er cambio project

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 /* video */ 
-let video=document.querySelector('video');
+/*let video = document.querySelector('video');
 let playBoton = document.getElementById("play");
 let pauseBoton= document.getElementById("pause");
 
 
-
-
-setTimeout(()=>{document.getElementById('showTime').innerHTML=` Duracion video : ${video.duration.toFixed(2)}`},1000);
+setTimeout(()=>{document.getElementById("showTime").innerHTML=`Duracion video : ${video.duration.toFixed(2)}`},1000);
 
 for (let i = 0; i < video.tim; i++) {
     console.log(video.currentTime)
@@ -38,4 +35,17 @@ function temaNoche() {
     let body = document.querySelector("body");
     body.classList.remove()
     body.classList.add("body-night")
-}
+}*/
+
+
+let video = document.querySelector('video');
+let play = document.getElementById('play');
+let pause = document.getElementById('pause');
+
+function reproducir() {
+    video.play()
+} 
+
+function pausar() {
+    video.pause()
+} 
