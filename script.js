5eb57c069e6d383c2c223370c11b7ec5ba649bfb
alan Calveyra <alancalveyra6@hotmail.com>
2022-08-01 03:45:49 -0300

    Last changes UTN Project

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
 /* video */ 
-/*let video = document.querySelector('video');
-let playBoton = document.getElementById("play");
-let pauseBoton= document.getElementById("pause");
-
-
-setTimeout(()=>{document.getElementById("showTime").innerHTML=`Duracion video : ${video.duration.toFixed(2)}`},1000);
-
-for (let i = 0; i < video.tim; i++) {
-    console.log(video.currentTime)
-    
-}
-
-playBoton.addEventListener('click',()=>{
-    video.play()
-
-   
-});
-
-pauseBoton.addEventListener('click',()=>{
-    video.pause();
-    flag=false;
-});
-
-function desplegar() {
-    let submenu = document.querySelector(".tema");
-    submenu.classList.toggle("tema-block")
-}
-function desptemaDia() {
-    let body = document.querySelector("body");
-    body.classList.remove()
-    body.classList.add("body-day")
-}
-function temaNoche() {
-    let body = document.querySelector("body");
-    body.classList.remove()
-    body.classList.add("body-night")
-}*/
 
 /*------------------------------JS ALAN CALVEYRA-------------------------------*/
 let video = document.querySelector('video');
@@ -50,13 +13,69 @@ function pausar() {
     video.pause()
 }
 
-let moverImagenes = ()=>{
-    let imagen1 = document.getElementById('img1');
-    let imagen2 = document.getElementById('img2');
-    let imagen3 = document.getElementById('img3');
-    let destino = document.getElementById('boxImg');
+/*------------------------JUEGO JS------------------------------*/
 
+let imagen1 = document.querySelector('#img1');
+let imagen2 = document.getElementById('img2');
+let imagen3 = document.getElementById('img3');
+let destino1 = document.getElementById('container1');
+let destino2 = document.getElementById('container2');
+let destino3 = document.getElementById('container3');
+
+imagen1.addEventListener("dragstart",(event)=>{
     
+    event.dataTransfer.setData('text',event.target.id);
+});
+
+destino1.addEventListener("dragover",(event)=>{
+        
+        event.preventDefault(); 
+});
+
+destino1.addEventListener("drop",(event)=>{
+    let imgSrc = event.dataTransfer.getData('text');
+
+    event.target.appendChild(document.getElementById(imgSrc));
+    document.querySelector('#p1').classList.add('none');
+});
+
+imagen2.addEventListener("dragstart",(event)=>{
+    
+    event.dataTransfer.setData('text',event.target.id);
+});
+
+destino2.addEventListener("dragover",(event)=>{
+        
+        event.preventDefault()
+});
+
+destino2.addEventListener("drop",(event)=>{
+    let imgSrc = event.dataTransfer.getData('text');
+
+    event.target.appendChild(document.getElementById(imgSrc));
+    document.querySelector('#p2').classList.add('none');
+});
+
+imagen3.addEventListener("dragstart",(event)=>{
+    
+    event.dataTransfer.setData('text',event.target.id);
+});
+
+destino3.addEventListener("dragover",(event)=>{
+        
+        event.preventDefault()
+});
+
+destino3.addEventListener("drop",(event)=>{
+    let imgSrc = event.dataTransfer.getData('text');
+
+    event.target.appendChild(document.getElementById(imgSrc));
+    document.querySelector('#p3').classList.add('none');
+});
+
+function reinicio() {
+    window.location.reload();
 }
-/* */
+iniciar()
+/*------------------------JUEGO JS------------------------------*/
 /*------------------------------JS ALAN CALVEYRA-------------------------------*/
